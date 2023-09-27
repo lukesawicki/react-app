b7d0251481bbd7f62ec71be1dc82131b07276b56
lukesawicki <lukaszsa1@gmail.com>
2023-09-27 19:40:02 +0200

    Passing Functions via Props first, in progress 1

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,28 @@
 import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 // import "./ListGroup.css";
 
-function ListGroup() {
-    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+// we will pass data to parametrise few things:
+// {items: [], heading: string}
+
+interface Props {
+    items: string[];
+    heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
     const [selectedIndex, setselectedIndex] = useState(-1);
+    // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
     return (
         <>
-            <h1>List</h1>
+            <h1>{heading}</h1>
             {items.length === 0 && <p>No item found</p>}
             <ul className="list-group">
                 {items.map((item, index) => (
                     <li
                         className={
                             selectedIndex === index
-                                ? "listg-group-item active"
+                                ? "list-group-item active"
                                 : "list-group-item"
                         }
                         key={item}
